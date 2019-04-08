e6fb58096c4dc8fafaafe3f54e4f5eefb6f51d27
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-04-08 16:14:06 -0400

    Https server for aws

--- frontend/httpsServer.py
+++ frontend/httpsServer.py
@@ -3,6 +3,6 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = BaseHTTPServer.HTTPServer(('54.225.0.100', 80), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
 httpd.serve_forever()
